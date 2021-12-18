4c5b90e34cf66f818d252cb2cff49620cc7cfc05
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-18 07:45:23 +0200

    add method to get data from repo

--- lib/business_logic/bloc/cubit/characters_cubit.dart
+++ lib/business_logic/bloc/cubit/characters_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:breaking_bad_series/data/models/character_model.dart';
+import 'package:breaking_bad_series/data/repository/characters_repositry.dart';
 import 'package:meta/meta.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  //ده هنا الاوبجكت البستقبل فيهو البيانات بتاعت الريبو 
+  final CharactersRepository charactersRepository;
+  CharactersCubit(this.charactersRepository) : super(CharactersInitial());
+
+  late List <Character> characters;
+  List<Character> getAllCharacters(){
+    charactersRepository.getAllCharacters().then((characters)  {
+      emit(CharactersLoaded(characters));
+      this.characters = characters;
+    });
+    return characters;
+  }
 }
