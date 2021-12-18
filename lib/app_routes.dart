58b89af0d14f26ef3937dbbe8ebd50faed35c2ad
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-18 12:34:27 +0200

    add cubit to screen

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,16 +1,35 @@
+import 'package:breaking_bad_series/business_logic/bloc/cubit/characters_cubit.dart';
 import 'package:breaking_bad_series/constants/strings.dart';
+import 'package:breaking_bad_series/data/repository/characters_repositry.dart';
+import 'package:breaking_bad_series/data/web_services/characters_web_services.dart';
 import 'package:breaking_bad_series/ui/screens/charactersDetails_screen.dart';
 import 'package:breaking_bad_series/ui/screens/characters_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AppRoutes{
- //this method to create routes btw screens by normal route => return type of Route
-  Route? generateRoute(RouteSettings settings){
-    switch (settings.name){
-      case charactersScreen :
-        return MaterialPageRoute(builder: (_)=>const CharactersScreen());
-      case charactersDetailsScreen :
-        return MaterialPageRoute(builder: (_)=>const CharactersDetailsScreen());
+class AppRoutes {
+  late CharactersRepository charactersRepository;
+  late CharactersCubit charactersCubit;
+
+  AppRoutes() {
+    charactersRepository = CharactersRepository(CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+
+  //this method to create routes btw screens by normal route => return type of Route
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case charactersScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (BuildContext context) =>
+               charactersCubit ,
+            child:const CharactersScreen(),
+          ),
+        );
+      case charactersDetailsScreen:
+        return MaterialPageRoute(
+            builder: (_) => const CharactersDetailsScreen());
     }
   }
 }
